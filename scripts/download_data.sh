a56db3c48128921763ad9eac1d1b45f252b4c873
shreyjasuja <sj4020@nyu.edu>
2024-04-03 01:54:35 +0000

    covost2 data

---
+++ scripts/download_data.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Check if a URL was passed as an argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# The URL from which to download the file, taken from the script's first argument
+url="$1"
+
+# Use basename and cut to extract the filename from the URL
+filename=$(basename "$url" | cut -d "?" -f 1)
+
+# Extract the part before ".tar.gz" for directory naming
+directory_name=$(echo "$filename" | sed 's/.tar.gz//' | sed 's/.tar//')
+
+# The base directory where you want to save the file
+base_directory="data"
+
+# Full path including the dynamically extracted directory name
+full_directory="${base_directory}/${directory_name}"
+
+# Create the directory if it doesn't exist
+mkdir -p "$full_directory"
+
+# Download the file with wget, saving it with the extracted filename in the specified directory
+wget "$url" -O "${full_directory}/${filename}"
+
+echo "Downloaded ${filename} to ${full_directory}/"
---
+++ scripts/extract_and_cleanup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+tar xvf ar/*.tar.gz -C ar
+rm ar/*.tar.gz*
+
+tar xvf ca/*.tar.gz -C ca
+rm ca/*.tar.gz*
+
+tar xvf cy/*.tar.gz -C cy
+rm cy/*.tar.gz*
+
+tar xvf de/*.tar.gz -C de
+rm de/*.tar.gz*
+
+tar xvf es/*.tar.gz -C es
+rm es/*.tar.gz*
+
+tar xvf et/*.tar.gz -C et
+rm et/*.tar.gz*
+
+tar xvf fa/*.tar.gz -C fa
+rm fa/*.tar.gz*
+
+tar xvf fr/*.tar.gz -C fr
+rm fr/*.tar.gz*
+
+tar xvf id/*.tar.gz -C id
+rm id/*.tar.gz*
+
+tar xvf it/*.tar.gz -C it
+rm it/*.tar.gz*
+
+tar xvf ja/*.tar.gz -C ja
+rm ja/*.tar.gz*
+
+tar xvf lv/*.tar.gz -C lv
+rm lv/*.tar.gz*
+
+tar xvf mn/*.tar.gz -C mn
+rm mn/*.tar.gz*
+
+tar xvf nl/*.tar.gz -C nl
+rm nl/*.tar.gz*
+
+tar xvf pt/*.tar.gz -C pt
+rm pt/*.tar.gz*
+
+tar xvf ru/*.tar.gz -C ru
+rm ru/*.tar.gz*
+
+tar xvf sl/*.tar.gz -C sl
+rm sl/*.tar.gz*
+
+tar xvf sv-SE/*.tar.gz -C sv-SE
+rm sv-SE/*.tar.gz*
+
+tar xvf ta/*.tar.gz -C ta
+rm ta/*.tar.gz*
+
+tar xvf tr/*.tar.gz -C tr
+rm tr/*.tar.gz*
+
+tar xvf zh-CN/*.tar.gz -C zh-CN
+rm zh-CN/*.tar.gz*
+
